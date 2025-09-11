1559644f2fdda08ad2d48dcaa1481f7284886023
AYUSHI <ayushiverma844@gmail.com>
2025-09-11 09:34:50 +0530

    loader page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Loader from "./pages/Loader";
+// import Signin from "./pages/Signin";
+// import Homepage from "./pages/Homepage";
 
-import Signin from "./pages/Signin";
-import Homepage from "./pages/Homepage";
 function App() {
   return (
-<Router>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/homepage" element={<Homepage />} />
-      </Routes>
-    </Router>
+    <div>
+      {/* Uncomment this section to enable routing */}
+      {/* 
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/homepage" element={<Homepage />} />
+        </Routes>
+      </Router> 
+      */}
+
+      {/* Render Loader page */}
+      <Loader />
+    </div>
   );
 }
+
 export default App;
