4cf36d4e57bc3b508ac938b40b519febade0d7ac
AYUSHI <ayushiverma844@gmail.com>
2025-09-16 09:52:21 +0530

    changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,21 @@
-require("dotenv").config();
-const mysql = require("mysql2/promise");
+const mysql = require("mysql");
+require("dotenv").config(); // Load .env variables
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-console.log("DB_USER is:", process.env.DB_USER);
+console.log("Password:", process.env.DB_PASSWORD);
 
-module.exports = pool;
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: ", err);
+    return;
+  }
+  console.log("Database connected successfully.");
+});
 
+module.exports = connection;
